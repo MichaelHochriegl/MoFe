67f635f5961fcaa4ea5e7aefa225bb4631857da6
MichaelHochriegl <michael@hochriegl.dev>
2023-08-24 17:51:18 +0200

    add tests

--- tests/MoFe.Api.Tests.Integration/Features/Movies/AddMovieTests.cs
+++ tests/MoFe.Api.Tests.Integration/Features/Movies/AddMovieTests.cs
@@ -3,6 +3,7 @@ using FastEndpoints;
 using FluentAssertions;
 using MoFe.Api.Features.Movies;
 using MoFe.Contracts.Features.Movies;
+using MoFe.Persistence.Entities.Movies;
 
 namespace MoFe.Api.Tests.Integration.Features.Movies;
 
@@ -30,5 +31,59 @@ public class AddMovieTests : IClassFixture<CustomWebAppFac>
         resp.StatusCode.Should().Be(HttpStatusCode.Created);
         result.Should().NotBeNull();
         result!.Id.Should().Be(1);
+    }
+    
+    [Fact]
+    public async Task Adding_already_existing_Movie_should_fail()
+    {
+        // Arrange
+        var movie = await _factory.SeedEntry(new Movie()
+        {
+            Name = "Ace Ventura",
+            ReleaseDate = DateOnly.Parse("1994.02.04")
+        });
+        
+        var request = new AddMovieRequest(movie.Name, movie.ReleaseDate);
+
+        // Act
+        var (resp, result) = await _client.POSTAsync<AddMovie, AddMovieRequest, ErrorResponse>(request);
+
+        // Assert
+        resp.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        result.Should().NotBeNull();
+        result!.Errors.Should()
+            .OnlyContain(e => e.Value.Contains($"Movie '{movie.Name} ({movie.ReleaseDate})' already present!"));
+    }
+    
+    [Fact]
+    public async Task Adding_new_Movie_with_empty_name_should_fail()
+    {
+        // Arrange
+        var request = new AddMovieRequest(string.Empty, DateOnly.Parse("1994.02.04"));
+
+        // Act
+        var (resp, result) = await _client.POSTAsync<AddMovie, AddMovieRequest, ErrorResponse>(request);
+
+        // Assert
+        resp.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        result.Should().NotBeNull();
+        result!.Errors.Should().OnlyContain(e =>
+            e.Key == "movieName" && e.Value.All(m => m == "'Movie Name' must not be empty."));
+    }
+    
+    [Fact]
+    public async Task Adding_new_Movie_with_old_release_date_should_fail()
+    {
+        // Arrange
+        var request = new AddMovieRequest("Really old movie", DateOnly.Parse("1899.12.31"));
+
+        // Act
+        var (resp, result) = await _client.POSTAsync<AddMovie, AddMovieRequest, ErrorResponse>(request);
+
+        // Assert
+        resp.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        result.Should().NotBeNull();
+        result!.Errors.Should().OnlyContain(e =>
+            e.Key == "releaseDate" && e.Value.All(m => m == "'Release Date' must be greater than or equal to '1/1/1900'."));
     }
 }
